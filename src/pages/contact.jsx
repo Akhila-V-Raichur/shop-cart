e6efedab66784f5360dc65a89237928e82fb06d9
Akhila V Raichur <akhilaraichur@gmail.com>
2024-05-22 23:45:58 +0530

    Initial commit

---
+++ src/pages/contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Contact = () => {
+  return (
+    <div>
+      <h1><b><i>Contact:</i></b></h1>
+      <div>
+        <h1>Phone no. : 999999999</h1>
+      </div>
+      <div>
+        <h1>email : sample@gmail.com</h1>
+      </div>
+    </div>
+  );
+};
